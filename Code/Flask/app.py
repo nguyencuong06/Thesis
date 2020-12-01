aa12d0d42d7ea0e5ab745061efe386b324962a20
Cuong Nguyen <nguyenhuycuong06@gmail.com>
2020-12-02 06:49:40 +0700

    Add files via upload

---
+++ Code/Flask/app.py
@@ -0,0 +1,72 @@
+import os
+import re
+import logging
+import requests
+#import yaml
+import datetime
+from flask import Flask, render_template, url_for, g, request, send_from_directory, abort, request_started, jsonify
+from flask_cors import CORS
+from flask_restful import Resource, reqparse
+from flask_restful_swagger_2 import Api, swagger, Schema
+from flask_json import FlaskJSON, json_response
+from neo4j import GraphDatabase, basic_auth
+from neo4j.exceptions import Neo4jError, ServiceUnavailable
+
+import neo4j.time
+
+DATABASE_USERNAME = ('neo4j')
+DATABASE_PASSWORD = ('salt-emitters-liver')
+DATABASE_URL = ('bolt://52.201.184.95:33649')
+driver = GraphDatabase.driver(DATABASE_URL, auth=basic_auth(DATABASE_USERNAME, str(DATABASE_PASSWORD)))
+app = Flask(__name__)
+api = Api(app, title='DemoNeo4j', api_version='1.1.2')
+app.config['SECRET_KEY'] = ('super secret guy')
+
+'''current_path = os.getcwd()
+print("Current directory is: " + current_path)
+path_to_yaml = os.path.join(current_path + 'deploy_web_config.yml')
+print("Path to yaml" + path_to_yaml)
+try:
+    with open('./deploy_web_config.yml', 'r') as c_file:
+        config = yaml.safe_load(c_file)
+except Exception as e:
+    print("Error reading  the config file")
+#set filename
+pipeline1_filename = config['file_names']['pipeline1_filename']
+pipeline2_filename = config['file_names']['pipeline2_filename']
+model_filename = config['file_names']['model_filename']
+
+#other params
+debug_on = config['general']['debug_on']
+logging_level = config['general']['logging_level']
+BATCH_SIZE = config['general']['BATCH_SIZE']
+
+def get_path(subpath):
+    rawpath = os.getcwd()
+    path = os.path.abspath(os.path.join(rawpath,'..',subpath))
+
+pipeline_path = get_path('pipeline')    
+pipeline1_path = os.path.join(pipeline_path, pipeline1_filename)
+pipeline2_path = os.path.join(pipeline_pth, pipeline2_filename)
+model_path = os.path.join(get_path('models'), models_filename)'''
+
+
+
+@app.route('/', methods=['GET'])
+def final():
+    #session =  driver.session()
+    #friends = session.read_transaction(get_data, "Cameron Crowe")        
+    return render_template('base.html')
+
+'''@app.route('/')
+def about():
+    #Get value from front-end
+    value = {}
+    value['head'] = request.args.get('head')
+    value['relation'] = request.args.get('relation')
+    value['tail'] = request.args.get('tail')
+    for ele in value:
+        logging.warning("value for "+ele+" is: "+str(value[ele]))'''
+
+if __name__ == "__main__":
+    app.run(debug=True)  
